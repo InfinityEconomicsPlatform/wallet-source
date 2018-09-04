1f2faa668152eb93cebfa46aefcbfd97b8cc9831
IEDevs <ie-devs@tuta.io>
2018-09-04 20:18:07 +0530

    updated icons

--- wallet_source-1.0.0-beta/source/src/app/module/account/lessors/lessors.component.ts
+++ wallet_source-1.0.0-beta/source/src/app/module/account/lessors/lessors.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as tableData from '../../../shared/data/smart-data-table';
-import { LocalDataSource } from 'ng2-smart-table';
 import {AccountService} from '../account.service';
 import {CommonService} from '../../../services/common.service';
 import {Router} from '@angular/router';
