88e3a447cd17cecdac5ab0b60a29992c14386ab0
IEDevs <ie-devs@tuta.io>
2018-09-19 21:36:41 +0530

    WIP: marketplace recent listing and purchases

--- wallet_source-1.0.0-beta/source/src/app/module/marketplace/marketplace.module.ts
+++ wallet_source-1.0.0-beta/source/src/app/module/marketplace/marketplace.module.ts
@@ -4,13 +4,21 @@ import { CommonModule } from '@angular/common';
 import { MarketplaceRoutingModule } from './marketplace-routing.module';
 import { StoreComponent } from './store/store.component';
 import { SharedModule } from '../../shared/shared.module';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { MarketplaceService } from './marketplace.service';
 
 @NgModule({
     imports: [
         CommonModule,
         MarketplaceRoutingModule,
         SharedModule,
+        NgxDatatableModule
     ],
-    declarations: [StoreComponent]
+    declarations: [
+        StoreComponent
+    ],
+    providers: [
+        MarketplaceService
+    ]
 })
 export class MarketplaceModule { }
--- wallet_source-1.0.0-beta/source/src/app/module/marketplace/store/store.component.ts
+++ wallet_source-1.0.0-beta/source/src/app/module/marketplace/store/store.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Page } from '../../../config/page';
 
 @Component({
-  selector: 'app-store',
-  templateUrl: './store.component.html',
-  styleUrls: ['./store.component.scss']
+    selector: 'app-store',
+    templateUrl: './store.component.html',
+    styleUrls: ['./store.component.scss']
 })
 export class StoreComponent implements OnInit {
+    recentListings: any[] = [];
+    page = new Page();
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
