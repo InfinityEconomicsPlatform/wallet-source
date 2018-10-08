f45e90ac327682495bf023b9dd808522a4331cc9
IEDevs <ie-devs@tuta.io>
2018-10-08 21:23:53 +0530

    WIP: list a product on marketplace.

--- wallet_source-1.0.0-beta/source/src/app/shared/sidebar/sidebar-routes.config.ts
+++ wallet_source-1.0.0-beta/source/src/app/shared/sidebar/sidebar-routes.config.ts
@@ -752,6 +752,16 @@ export const ROUTES: RouteInfo[] = [
                 badgeClass: '',
                 isExternalLink: false,
                 submenu: []
+            },
+            {
+                path: '/marketplace/list-a-product',
+                title: 'List A Product',
+                icon: '',
+                class: '',
+                badge: '',
+                badgeClass: '',
+                isExternalLink: false,
+                submenu: []
             }
         ],
         isSwapp: true,
