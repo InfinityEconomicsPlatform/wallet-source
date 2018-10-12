4c6770ba36bc947dae8b9527c9a2ecf8bece8673
IEDevs <ie-devs@tuta.io>
2018-10-12 20:18:31 +0530

    Added translation keys for navbar tools pages, service fees, validate signature, parse transaction, broadcast transaction, calculate hash, chain statistics, generate signature, and user guide pages

--- wallet_source-1.0.0-beta/source/src/app/module/tools-pages/service-fees/service-fees.component.ts
+++ wallet_source-1.0.0-beta/source/src/app/module/tools-pages/service-fees/service-fees.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-service-fees',
   templateUrl: './service-fees.component.html',
-  styleUrls: ['./service-fees.component.scss']
+  styleUrls: ['./service-fees.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ServiceFeesComponent implements OnInit {
 
