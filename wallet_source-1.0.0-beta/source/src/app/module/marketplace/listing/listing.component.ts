76565249aa8c6fb0d08944b2858867a5f28bbaf8
IEDevs <ie-devs@tuta.io>
2018-10-15 14:15:20 +0530

    WIP: listing of my products

--- wallet_source-1.0.0-beta/source/src/app/module/marketplace/listing/listing.component.ts
+++ wallet_source-1.0.0-beta/source/src/app/module/marketplace/listing/listing.component.ts
@@ -8,7 +8,27 @@ import { Page } from '../../../config/page';
 })
 export class ListingComponent implements OnInit {
     page = new Page();
-    products_listed: any[] = [];
+    products_listed: any[] = [
+        {
+            "seller": "15323192282528158131",
+            "quantity": 2,
+            "goods": "11813734897437346473",
+            "description": "Testing the DGS.",
+            "sellerRS": "XIN-L6FM-89WK-VK8P-FCRBB",
+            "delisted": false,
+            "parsedTags": [
+                "test",
+                "product",
+                "tag"
+            ],
+            "tags": "test, product, tag, extra",
+            "priceTQT": "200000000",
+            "numberOfPublicFeedbacks": 0,
+            "name": "Test Product",
+            "numberOfPurchases": 0,
+            "timestamp": 31436434
+        }
+    ];
 
     constructor() {
 
