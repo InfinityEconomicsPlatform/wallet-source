f981ec9de94ed7fefadd25f3d591c4a1bd81beab
IEDevs <ie-devs@tuta.io>
2018-10-15 14:21:05 +0530

    WIP: listing of my products

--- wallet_source-1.0.0-beta/source/src/app/module/marketplace/listing/listing.component.ts
+++ wallet_source-1.0.0-beta/source/src/app/module/marketplace/listing/listing.component.ts
@@ -53,4 +53,5 @@ export class ListingComponent implements OnInit {
         DataStoreService.set('marketplace_product', product);
         this.router.navigateByUrl('/marketplace/remove-product');
     }
+
 }
