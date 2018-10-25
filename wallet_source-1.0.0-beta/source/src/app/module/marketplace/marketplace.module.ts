f63bf79e2d0540bccc384b8bc7ea49e056725113
IEDevs <ie-devs@tuta.io>
2018-10-25 21:21:17 +0530

    Change Price, Quantity & Delete Product UI

--- wallet_source-1.0.0-beta/source/src/app/module/marketplace/marketplace.module.ts
+++ wallet_source-1.0.0-beta/source/src/app/module/marketplace/marketplace.module.ts
@@ -14,15 +14,12 @@ import { PurchasedComponent } from './purchased/purchased.component';
 import { ListAProductComponent } from './list-a-product/list-a-product.component';
 import { SearchComponent } from './search/search.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-<<<<<<< HEAD
 import { ChangePriceComponent } from './listing/change-price/change-price.component';
 import { ChangeQuantityComponent } from './listing/change-quantity/change-quantity.component';
 import { DeleteProductComponent } from './listing/delete-product/delete-product.component';
 import { PendingOrdersComponent } from './pending-orders/pending-orders.component';
 import { CompletedOrdersComponent } from './completed-orders/completed-orders.component';
 import { DeliverComponent } from './deliver/deliver.component';
-=======
->>>>>>> Added product details page
 
 @NgModule({
     imports: [
@@ -39,7 +36,6 @@ import { DeliverComponent } from './deliver/deliver.component';
         ListingComponent,
         PurchasedComponent,
         ListAProductComponent,
-<<<<<<< HEAD
         SearchComponent,
         ProductDetailsComponent,
         ChangePriceComponent,
@@ -48,18 +44,6 @@ import { DeliverComponent } from './deliver/deliver.component';
         PendingOrdersComponent,
         CompletedOrdersComponent,
         DeliverComponent
-=======
-        ProcessWithoutApprovalComponent,
-        DefferedHeightComponent,
-        FixedAccountsComponent,
-        AmountComponent,
-        AssetsComponent,
-        CurrenciesComponent,
-        TransactionHashComponent,
-        SecretHashComponent,
-        SearchComponent,
-        ProductDetailsComponent
->>>>>>> Added product details page
     ],
     providers: [
         MarketplaceService
