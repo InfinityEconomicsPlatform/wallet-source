999cdbb429f3ed9a9c6997a4c50c005edff4cf66
IEDevs <ie-devs@tuta.io>
2018-11-02 20:51:02 +0530

    WIP: Deliver the product page

--- wallet_source-1.0.0-beta/source/src/app/module/marketplace/deliver/deliver.component.ts
+++ wallet_source-1.0.0-beta/source/src/app/module/marketplace/deliver/deliver.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { MarketplaceService } from '../marketplace.service';
+import { DataStoreService } from 'app/services/data-store.service';
 
 @Component({
-  selector: 'app-deliver',
-  templateUrl: './deliver.component.html',
-  styleUrls: ['./deliver.component.scss']
+    selector: 'app-deliver',
+    templateUrl: './deliver.component.html',
+    styleUrls: ['./deliver.component.scss']
 })
 export class DeliverComponent implements OnInit {
+    product: any = {};
+    constructor(public router: Router,
+        private marketplaceService: MarketplaceService,
+        public location: Location) {
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.product = DataStoreService.get('marketplace_product');
+        if (typeof (this.product) == "undefined") {
+            this.location.back();
+        } else {
+            this.marketplaceService.getDGSGood(this.product.goods).subscribe((success: any) => {
+                this.product = success;
+            }, (error) => {
+                console.log("Product Details: ", error);
+            });
+        }
+    }
 
 }
