4ad30a603fb2786c39fc480bb2f2ab3680e88a51
IEDevs <ie-devs@tuta.io>
2018-11-28 20:40:20 +0530

    Image Upload with validations

--- wallet_source-1.0.0-beta/source/src/app/module/marketplace/list-a-product/list-a-product.component.ts
+++ wallet_source-1.0.0-beta/source/src/app/module/marketplace/list-a-product/list-a-product.component.ts
@@ -3,6 +3,8 @@ import { MarketplaceService } from "../marketplace.service";
 import { Router } from '@angular/router';
 import { AmountToQuantPipe } from 'app/pipes/amount-to-quant.pipe';
 import * as alertFunction from "../../../shared/data/sweet-alerts";
+import { HttpClient } from '@angular/common/http';
+import { AppConstants } from 'app/config/constants';
 
 @Component({
     selector: 'app-list-a-product',
@@ -22,10 +24,12 @@ export class ListAProductComponent implements OnInit {
         messageFile: '',
         messageIsPrunable: true
     };
+    selectedFile: File;
 
     constructor(public router: Router,
         private marketplaceService: MarketplaceService,
-        private amountToQuant: AmountToQuantPipe) {
+        private amountToQuant: AmountToQuantPipe,
+        public http: HttpClient) {
 
     }
 
@@ -33,12 +37,38 @@ export class ListAProductComponent implements OnInit {
 
     }
 
-    putProductForSale(product) {
+    onFileChange(event) {
+        if ((event.target.files[0].size / 1024) < 20) {
+            this.selectedFile = event.target.files[0];
+        } else {
+            alertFunction.InfoAlertBox(
+                "Error",
+                "Image size should less than 20KBs",
+                "OK",
+                'error'
+            ).then((isConfirm: any) => {
+                event.target.value = "";
+            });
+        }
+    }
+
+    putProductForSale(product, productImage) {
         let priceTQT = this.amountToQuant.transform(product.priceTQT);
         let feeTQT = this.amountToQuant.transform(product.feeTQT);
-        let messageFile = '';
+        let formdata = new FormData();
+        formdata.append('messageFile', this.selectedFile, this.selectedFile.name);
+        formdata.append('requestType', 'dgsListing');
+        formdata.append('secretPhrase', product.secretPhrase);
+        formdata.append('name', product.name);
+        formdata.append('description', product.description);
+        formdata.append('tags', product.tags);
+        formdata.append('quantity', product.quantity);
+        formdata.append('priceTQT', priceTQT);
+        formdata.append('feeTQT', feeTQT);
+        formdata.append('messageIsPrunable', product.messageIsPrunable);
+        formdata.append('deadline', '60');
 
-        this.marketplaceService.dgsListing(product.secretPhrase, product.name, product.description, product.tags, product.quantity, priceTQT, feeTQT, messageFile, product.messageIsPrunable).subscribe((success: any) => {
+        this.http.post(AppConstants.marketPlaceConfig.apiUrl + '/' + AppConstants.pollConfig.pollEndPoint, formdata).subscribe((success: any) => {
             if (!success.errorCode) {
                 alertFunction.InfoAlertBox(
                     "Success",
@@ -53,8 +83,11 @@ export class ListAProductComponent implements OnInit {
                         priceTQT: 0,
                         quantity: 0,
                         feeTQT: 1,
-                        secretPhrase: ''
+                        secretPhrase: '',
+                        messageFile: '',
+                        messageIsPrunable: true
                     };
+                    productImage.value = "";
                 });
             } else {
                 alertFunction.InfoAlertBox(
@@ -66,6 +99,6 @@ export class ListAProductComponent implements OnInit {
             }
         }, (error) => {
             console.log("List a Product:", error);
-        })
+        });
     }
 }
